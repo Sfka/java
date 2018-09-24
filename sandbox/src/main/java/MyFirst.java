ce2e3565837ff64d4fe708ff156cc38be93abf7c
Sfka <v9226132634@gmail.com>
2018-09-24 21:59:13 +0500

    DZ_2_ispr

--- sandbox/src/main/java/MyFirst.java
+++ sandbox/src/main/java/MyFirst.java
@@ -2,8 +2,8 @@ public class MyFirst {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        Point p1 = new Point(10, 5);
-        Point p2 = new Point(5, 10);
-        System.out.println("Расстояние = " + p1.distance(p2));
+        Point p1 = new Point(10,5);
+        Point p2 = new Point(5,10);
+        System.out.println("Расстояние между p1 и p2 = " + p1.distance(p2));
     }
 }
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -1,14 +1,14 @@
 public class Point {
-    public Point p1;
-    public Point p2;
+    public double p1;
+    public double p2;
 
-    public Point(p1,p2) {
+    public Point(double p1, double p2) {
         this.p1 = p1;
         this.p2 = p2;
     }
 
     public double distance(Point p2) {
-        return Math.sqrt(Math.pow(p2.getX() - p1.getX(), 2) + Math.pow(p2.getY() - p1.getY(), 2));
+        return Math.sqrt(Math.pow(p2.p1-this.p1, 2) + Math.pow(p2.p2-this.p2, 2));
     }
 }
 
