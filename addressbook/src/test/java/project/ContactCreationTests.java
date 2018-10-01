d1157ff2774cae2213f1c4ddd7d8444bb74512ae
Sfka <v9226132634@gmail.com>
2018-10-01 20:38:31 +0500

    contact
    pered sozdaniem contactdata

--- addressbook/src/test/java/project/ContactCreationTests.java
+++ addressbook/src/test/java/project/ContactCreationTests.java
@@ -8,43 +8,54 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.testng.Assert.fail;
 
 public class ContactCreationTests {
   private WebDriver wd;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
-    baseUrl = "https://www.katalon.com/";
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    login("admin", "secret");
   }
+
+    private void login(String username, String password) {
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//input[@value='Login']")).click();
+    }
 
   @Test
-  public void testContactCreationTests() throws Exception {
-    wd.get("http://localhost/addressbook/");
-    wd.findElement(By.linkText("add new")).click();
-    wd.findElement(By.cssSelector("input[name=\"firstname\"]")).clear();
-    wd.findElement(By.cssSelector("input[name=\"firstname\"]")).sendKeys("Ivan");
-    wd.findElement(By.cssSelector("input[name=\"lastname\"]")).clear();
-    wd.findElement(By.cssSelector("input[name=\"lastname\"]")).sendKeys("Ivanov");
-    wd.findElement(By.cssSelector("input[name=\"mobile\"]")).clear();
-    wd.findElement(By.cssSelector("input[name=\"mobile\"]")).sendKeys("123456789");
-    wd.findElement(By.cssSelector("input[name=\"email\"]")).clear();
-    wd.findElement(By.cssSelector("input[name=\"email\"]")).sendKeys("1234@mail.com");
-    wd.findElement(By.cssSelector("input[name=\"submit\"]")).click();
+  public void testContactCreation() {
+      initContactCreation();
+      fillContactForm();
+      submitContactCreation();
   }
+    private void initContactCreation() {
+        wd.findElement(By.linkText("add new")).click();
+    }
 
-  @AfterClass(alwaysRun = true)
+    private void submitContactCreation() {
+        wd.findElement(By.cssSelector("input[name=\"submit\"]")).click();
+    }
+
+    private void fillContactForm() {
+        wd.findElement(By.cssSelector("input[name=\"firstname\"]")).clear();
+        wd.findElement(By.cssSelector("input[name=\"firstname\"]")).sendKeys("Ivan");
+        wd.findElement(By.cssSelector("input[name=\"lastname\"]")).clear();
+        wd.findElement(By.cssSelector("input[name=\"lastname\"]")).sendKeys("Ivanov");
+        wd.findElement(By.cssSelector("input[name=\"mobile\"]")).clear();
+        wd.findElement(By.cssSelector("input[name=\"mobile\"]")).sendKeys("123456789");
+        wd.findElement(By.cssSelector("input[name=\"email\"]")).clear();
+        wd.findElement(By.cssSelector("input[name=\"email\"]")).sendKeys("1234@mail.com");
+    }
+
+
+    @AfterClass(alwaysRun = true)
   public void tearDown() throws Exception {
     wd.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
   }
 
   private boolean isElementPresent(By by) {
@@ -62,21 +73,6 @@ public class ContactCreationTests {
       return true;
     } catch (NoAlertPresentException e) {
       return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = wd.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
     }
   }
 }
