95b8a8fa3fbe0e966aee0bca631f13a365b74298
Sfka <v9226132634@gmail.com>
2018-10-12 20:39:28 +0500

    lec 3.5

--- addressbook/src/test/java/project/tests/GroupCreationTests.java
+++ addressbook/src/test/java/project/tests/GroupCreationTests.java
@@ -10,7 +10,7 @@ public class GroupCreationTests extends TestBase{
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test1-2", "test1-3"));
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
   }
