e41cf478a0394555029631aaacca6952539c82f6
Sfka <v9226132634@gmail.com>
2018-10-28 21:37:26 +0500

    lec 4.3

--- addressbook/src/test/java/project/tests/GroupModificationTests.java
+++ addressbook/src/test/java/project/tests/GroupModificationTests.java
@@ -1,6 +1,7 @@
 package project.tests;
 
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import project.model.GroupData;
 
@@ -9,6 +10,7 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification(){
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         if (! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
@@ -17,5 +19,7 @@ public class GroupModificationTests extends TestBase {
         app.getGroupHelper().fillGroupForm(new GroupData("test1", "test1-2", "test1-3"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before);
     }
 }
