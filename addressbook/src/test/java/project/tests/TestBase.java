5719e979a688c78864af9fb3a6d5067fe3b0f57f
Sfka <v9226132634@gmail.com>
2018-11-12 08:16:05 +0500

    lec 6.10

--- addressbook/src/test/java/project/tests/TestBase.java
+++ addressbook/src/test/java/project/tests/TestBase.java
@@ -1,14 +1,16 @@
 package project.tests;
 
-import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import project.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+
 
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite //suite (vsegda odin) - test - class - method
     public void setUp() throws Exception {
